a306cd02ae8de799a5186d0106147100bbb33f29
Nick Meinhold <nick.meinhold@gmail.com>
2021-04-24 13:17:06 +1000

    Add getProjectId to ProjectMetadataService

--- lib/src/middleware.dart
+++ lib/src/middleware.dart
@@ -2,19 +2,24 @@ import 'dart:async';
 
 import 'package:shelf/shelf.dart';
 
+import 'services/project_metadata_service.dart';
+
 /// Top level function that returns a [Middleware] that authenticates Firebase
 /// users.
 Middleware firebaseAuthMiddelware(
         {FutureOr<Response> Function(Object, StackTrace)? errorHandler}) =>
     createMiddleware(
-        requestHandler: (request) {
-          // final projectId = await projectMetadataService.getProjectId();
+        requestHandler: (request) async {
+          final service = ProjectMetadataService();
+          final projectId = service.getProjectId();
+          service.close();
 
           request.headers['authorization'];
 
           // final claimSet = verifyJwtRSA256Signature(token, key);
-          // verifyJwtHS256Signature(token, hmacKey)
           // claimSet.validate(issuer: 'teja', audience: 'audience1.example.com');
+
+          // request.change(headers: {uid: userId});
 
           return null; // null means the request is sent to the inner [Handler]
         },
--- lib/src/services/project_metadata_service.dart
+++ lib/src/services/project_metadata_service.dart
@@ -1,5 +1,41 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart';
+
 class ProjectMetadataService {
-  // String getProjectId() {
-  //   if (projectId == null) throw 'Project Id was not found.';
-  // }
+  static const _requestHeaders = {'Metadata-Flavor': 'Google'};
+  final _uri =
+      Uri.parse('http://metadata.google.internal/computeMetadata/v1/project/');
+  final _client = Client();
+
+  /// Retrieves projectId from a GCP metadata server, if executed where
+  /// one is a available (eg. Cloud Run).
+  ///
+  /// Otherwise throws an exception.
+  Future<String> getProjectId() async {
+    if (!onCloudRun) throw 'Ran getProjectId() from unsupported environment';
+
+    final response = await _client.get(_uri, headers: _requestHeaders);
+
+    if (response.statusCode != 200) {
+      throw HttpException(
+        '${response.statusCode} : ${response.body}',
+        uri: _uri,
+      );
+    }
+
+    final json = jsonDecode(response.body);
+    final projectId = json['projectId'] as String?;
+
+    if (projectId == null) throw 'Project Id was not found.';
+
+    return projectId;
+  }
+
+  bool get onCloudRun => Platform.environment.containsKey('K_SERVICE');
+
+  void close() {
+    _client.close();
+  }
 }
